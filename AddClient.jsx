476c4a6a8534cb77418b10925cea79d01b31cb48
AngelSuarezKea <asuarez@kea.mx>
2022-11-28 15:26:46 -0600

    added full clients module

--- AddClient.jsx
+++ AddClient.jsx
@@ -1,9 +1,311 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import Input from './hooks/Input';
+import PrimaryButton from './hooks/PrimaryButton';
+import SecondaryButton from './hooks/SecondaryButton';
+import SelectCustom from './hooks/SelectCustom';
+import { rfcRegex, emailRegex, zipRegex, containsNumbersRegex } from './hooks/useRegex';
+//import { getTaxRegimes } from '../../apiClient/operations/catalogs';
+import Alert from './hooks/Alert';
+//import { createCustomer, updateCustomer } from '../../apiClient/operations/customerOperations';
 
-function AddClient() {
+function AddClient({ organizationId, client, onAdded, onCancel, setIsLoading }) {
+
+    const [regimeList, setRegimeList] = useState(null);
+    const [regime, setRegime] = useState(client?.regime);
+    const [filteredRegimeList, setFilteredRegimeList] = useState(null);
+    const [error, setError] = useState(null);
+    const [errors, setErrors] = useState(null);
+
+   /*useEffect(() => {
+        const getRegimeList = async () => {
+            try {
+                if (!regimeList) {
+                    const res = await getTaxRegimes();
+                    setFilteredRegimeList(res.data.tax_regimes);
+                    setRegimeList(res.data.tax_regimes);
+                }
+            } catch (e) {
+                console.log(e);
+            }
+        }
+        getRegimeList();
+    }, [regimeList]);*/
+
+    const filterRegimeList = (regimeList, taxPersonType) => {
+        if (!regimeList) return null;
+        if (taxPersonType) {
+            return regimeList.reduce((filtered, item) => {
+                if (item.type.includes(taxPersonType)) {
+                    filtered.push({
+                        id: item.regime_id,
+                        name: item.name
+                    });
+                }
+                return filtered;
+            }, []);
+        } else {
+            return regimeList.map((regime) => ({
+                id: regime.regime_id,
+                name: regime.name
+            }))
+        }
+    }
+
+    useEffect(() => {
+        //Filter regime list by rfc
+        let taxPersonType;
+        if (client?.tax_id?.length >= 4) taxPersonType = containsNumbersRegex(client.tax_id.slice(0,4)) ? 'enterprise' : 'personal';
+        setFilteredRegimeList(filterRegimeList(regimeList, taxPersonType));
+        setRegime(client?.tax_system?.name);
+    }, [regimeList, client]);
+
+    useEffect(() => {
+        if (!errors) return;
+        setErrors({ ...errors, regime: null });
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [regime]);
+
+    const handleName = e => {
+        const name = e.target.value;
+        if (!errors) return;
+        if (name == null || name == "") {
+            setErrors({ ...errors, name: "Ingresa el nombre o razón social" });
+        } else if (name.length < 3) {
+            setErrors({ ...errors, name: "Ingresa correctamente el nombre o razón social" });
+        } else {
+            setErrors({ ...errors, name: null });
+        }
+    }
+
+    const handleRfc = e => {
+        const rfc = e.target.value.toUpperCase();
+        e.target.value = rfc;
+        //Filter regime list by rfc
+        let taxPersonType;
+        if (rfc?.length >= 4) taxPersonType = containsNumbersRegex(rfc.slice(0,4)) ? 'enterprise' : 'personal';
+        setFilteredRegimeList(filterRegimeList(regimeList, taxPersonType));
+        if (regime && taxPersonType && !(regimeList.find(item => item.name == regime)?.type?.includes(taxPersonType))) setRegime(null);
+
+        if (!errors) return;
+        if (rfc == null || rfc == "") {
+            setErrors({ ...errors, rfc: "Ingresa el RFC" });
+        } else if (!rfcRegex(rfc)) {
+            setErrors({ ...errors, rfc: "Ingresa correctamente el RFC" });
+        } else {
+            setErrors({ ...errors, rfc: null });
+        }
+    }
+
+    const handleEmail = e => {
+        const email = e.target.value;
+        if (!errors) return;
+        if (email == null || email == "") {
+            setErrors({ ...errors, email: "Ingresa el correo electrónico" });
+        } else if (!emailRegex(email)) {
+            setErrors({ ...errors, email: "Ingresa correctamente el correo electrónico" });
+        } else {
+            setErrors({ ...errors, email: null });
+        }
+    }
+
+    const handleZip = e => {
+        const zip = e.target.value;
+        if (!errors) return;
+        if (zip == null || zip == "") {
+            setErrors({ ...errors, zip: "Ingresa el código postal" });
+        } else if (!zipRegex(zip)) {
+            setErrors({ ...errors, zip: "Ingresa correctamente el código postal" });
+        } else {
+            setErrors({ ...errors, zip: null });
+        }
+    }
+
+    const onSubmit = async e => {
+        e.preventDefault();
+        const name = e.target.elements.name.value;
+        const rfc = e.target.elements.rfc.value;
+        const email = e.target.elements.email.value;
+        const zip = e.target.elements.zip.value;
+
+        let err = {};
+
+        if (name == null || name == "") {
+            err.name = "Ingresa el nombre o razón social";
+        } else if (name.length < 8) {
+            err.name = "Ingresa correctamente el nombre o razón social";
+        }
+
+        if (rfc == null || rfc == "") {
+            err.rfc = "Ingresa el RFC";
+        } else if (!rfcRegex(rfc)) {
+            err.rfc = "Ingresa correctamente el RFC";
+        }
+
+        if (regime == null || regime == "") err.regime = "Selecciona el régimen fiscal";
+
+        if (email == null || email == "") {
+            err.email = "Ingresa el correo electrónico";
+        } else if (!emailRegex(email)) {
+            err.email = "Ingresa correctamente el correo electrónico";
+        }
+
+        if (zip == null || zip == "") {
+            err.zip = "Ingresa el código postal";
+        } else if (!zipRegex(zip)) {
+            err.zip = "Ingresa correctamente el código postal";
+        }
+
+        if (Object.keys(err).length === 0) {
+            const regimeId = regimeList?.find(e => e.name == regime)?.regime_id;
+            const data = {
+                id: client?.id,
+                name,
+                rfc,
+                regimeId,
+                email,
+                zip
+            }
+            if (client) {
+                editCustomer(data, organizationId);
+            } else {
+                addCustomer(data, organizationId);
+            }
+        } else {
+            setErrors(err);
+        }
+    }
+
+    const addCustomer = async (client, organizationId) => {
+        setIsLoading(true);
+        setError(null);
+        try {
+            /*const customer = await createCustomer(
+                client.name,
+                client.regimeId,
+                client.rfc,
+                client.email,
+                client.zip,
+                organizationId
+            );
+            if (onAdded) onAdded(customer);*/
+            setIsLoading(false);
+        } catch (e) {
+            setIsLoading(false);
+            setError(e.messageToUser);
+        }
+    }
+
+    const editCustomer = async (client, organizationId) => {
+        setIsLoading(true);
+        setError(null);
+        try {
+            /*const customer = await updateCustomer(
+                client.name,
+                client.regimeId,
+                client.rfc,
+                client.email,
+                client.zip,
+                organizationId,
+                client.id
+            );
+            if (onAdded) onAdded(customer);*/
+            setIsLoading(false);
+        } catch (e) {
+            setIsLoading(false);
+            setError(e.messageToUser)
+        }
+    }
+
+    const cancel = e => {
+        e.preventDefault();
+        if (onCancel) onCancel();
+    }
+
     return (
-        <div>AddClient</div>
+        <form className="w-full" onSubmit={onSubmit}>
+            <div className="space-y-8">
+                <div className="">
+                    <div>
+                        <h5 className='font-bold'>{client ? 'Editar cliente' : 'Agregar nuevo cliente'}</h5>
+                        <div className='mt-5 -mx-6 border-t-2 border-gray-300' />
+                    </div>
+                    <div className="py-4 grid grid-cols-1 gap-y-4 gap-x-4 max-h-screen sm:max-h-[calc(100vh-300px)] overflow-y-auto -mx-6 px-6">
+                        {error &&
+                            <div className='mb-4 top-0 sticky z-50'>
+                                <Alert title={error} show={error != null} onClose={() => setError(null)} />
+                            </div>
+                        }
+                        <Input
+                            label='Nombre o razón social'
+                            type='text'
+                            id='name'
+                            onChange={handleName}
+                            needed
+                            defaultValue={client?.legal_name}
+                            error={errors?.name} />
+                        <Input
+                            label='RFC'
+                            type='text'
+                            id='rfc'
+                            onChange={handleRfc}
+                            needed
+                            maxLength={13}
+                            defaultValue={client?.tax_id}
+                            error={errors?.rfc} />
+                        <SelectCustom
+                            label='Régimen fiscal'
+                            id='regime'
+                            needed
+                            value={regime}
+                            setValue={setRegime}
+                            listOptions={filteredRegimeList}
+                            error={errors?.regime} />
+                        <Input
+                            label='Correo electrónico'
+                            type='email'
+                            id='email'
+                            onChange={handleEmail}
+                            needed
+                            defaultValue={client?.email}
+                            error={errors?.email} />
+                        <Input
+                            label='Código postal'
+                            type='text'
+                            inputMode='numeric'
+                            id='zip'
+                            onChange={handleZip}
+                            needed
+                            maxLength={5}
+                            defaultValue={client?.address?.zip}
+                            error={errors?.zip} />
+                    </div>
+                </div>
+            </div>
+            <div>
+                <div className='mb-5 -mx-6 border-t-2 border-gray-300' />
+                <div className="flex justify-end gap-5">
+                    <SecondaryButton
+                        type="button"
+                        onClick={cancel}>
+                        Cancelar
+                    </SecondaryButton>
+                    <PrimaryButton
+                        type="submit">
+                        {client ? 'Guardar' : 'Guardar cliente'}
+                    </PrimaryButton>
+                </div>
+            </div>
+        </form>
     )
 }
 
-export default AddClient
+AddClient.propTypes = {
+    organizationId: PropTypes.string,
+    client: PropTypes.object,
+    onAdded: PropTypes.func,
+    onCancel: PropTypes.func,
+    setIsLoading: PropTypes.func
+}
+
+export default AddClient;
--- Clients.jsx
+++ Clients.jsx
@@ -1,9 +1,213 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import { PlusIcon, SearchIcon, UsersIcon } from '@heroicons/react/solid';
+import Modal from './hooks/Modal';
+import PrimaryButton from './hooks/PrimaryButton';
+import AddClient from './AddClient';
+import Input from './hooks/Input';
+import Table from './hooks/Table';
+import EmptyState from './hooks/EmptyState';
+import Pagination from './hooks/Pagination';
+import Alert from './hooks/Alert';
+//import { getCustomers, deleteCustomer } from '../../apiClient/operations/customerOperations';
+import useScrollPosition from './hooks/useScrollPosition';
+import usePagination from './hooks/usePagination';
+import Loader from './hooks/Loader';
 
-function Clients() {
+const tableColumns = [
+    { heading: 'Nombre o razón social', value: 'legal_name' },
+    { heading: 'Correo electrónico', value: 'email' },
+    { heading: 'RFC', value: 'tax_id' },
+    { heading: 'Régimen fiscal', value: 'tax_system.name' }
+]
+
+const itemsLimitInTable = 20;
+
+function Clients({ user }) {
+
+    const [clients, setClients] = useState([]);
+    const [openClientModal, setOpenClientModal] = useState(false);
+    const [currentPage, setCurrenPage] = useState(1);
+    const [pagination, setPagination] = useState(null);
+    const [editClient, setEditClient] = useState(null);
+    const [isLoadingData, setIsLoadingData] = useState(false);
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState(null);
+    const [clientsList, setClientsList] = useState([]);
+    const setScrollPosition = useScrollPosition();
+    const paginate = usePagination();
+
+    /*const getCustomerList = async (limit, page) => {
+        setIsLoadingData(true);
+        setScrollPosition(0);
+        try {
+            const res = await getCustomers(user.organizationId, limit, page);
+            setClients(res?.data?.customers);
+            setPagination(paginate(res.total_items, itemsLimitInTable, res.total_pages));
+            setIsLoadingData(false);
+        } catch (e) {
+            setIsLoadingData(false);
+            setError(e.messageToUser);
+        }
+    }*/
+
+    /*useEffect(() => {
+        getCustomerList(itemsLimitInTable, currentPage);
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [currentPage]);*/
+
+    const handleDeleteClient = async item => {
+        let tempClients = clients;
+        try {
+            setIsLoadingData(true);
+            setClients([]);
+            //await deleteCustomer(user?.organizationId, item.id);
+            //getCustomerList(itemsLimitInTable, currentPage);
+        } catch (e) {
+            setClients(tempClients);
+            setIsLoadingData(false);
+            setError(e.messageToUser);
+        }
+    }
+
+    const handleEditClient = item => {
+        const client = clients.find(client => client.id === item.id);
+        setEditClient(client);
+        setOpenClientModal(true);
+    }
+
+    const actions = [
+        {
+            name: 'Editar',
+            action: handleEditClient
+        },
+        {
+            name: 'Eliminar',
+            action: handleDeleteClient
+        }
+    ];
+
+    const onClientAdded = async clientAdded => {
+        setOpenClientModal(false);
+        if (editClient) {
+            setEditClient(null);
+            let updatedClients = clients.filter(client => client.id !== clientAdded.id);
+            setClients([
+                clientAdded,
+                ...updatedClients
+            ]);
+        } else {
+            setClients([]);
+            getCustomerList(itemsLimitInTable, currentPage);
+        }
+    }
+
+    const onCancelAdded = () => {
+        if (editClient) setEditClient(null);
+        setOpenClientModal(false);
+    }
+
+    const handleSearch = value => {
+        const filtered = clients.filter(item => {
+            if (value == '') {
+                return item
+            } else if ( item.legal_name.toLocaleLowerCase().includes(value.toLocaleLowerCase()) || item.email.toLocaleLowerCase().includes(value.toLocaleLowerCase()) || item.tax_id.toLocaleLowerCase().includes(value.toLocaleLowerCase()) ) {
+                return item
+            } else {
+                return ''
+            }
+        });
+        setClientsList(filtered);
+    }
+
+    useEffect(() => {
+        setClientsList(clients);
+        return () => {
+            setClientsList([]);
+        }
+    }, [clients]);
+
     return (
-        <div>Clients.</div>
+        <>
+            <Loader show={isLoading} />
+            {/* Calculate min height -> screen height minus top bar height*/}            
+            <div className='min-h-full md:min-h-[calc(100vh-4rem)] bg-gray-200/70 px-4 lg:px-9 pt-6 pb-10'>
+                {error &&
+                    <div className='mb-5 top-[5.5rem] sticky z-50'>
+                        <Alert title={error} show={error != null} onClose={() => setError(null)} />
+                    </div>
+                }
+                <div className='flex justify-between'>
+                    <h2 className='text-3xl font-bold text-gray-900'>
+                        Clientes
+                    </h2>
+                    {clients.length > 0 && (
+                        <PrimaryButton
+                            onClick={() => setOpenClientModal(true)}>
+                            <PlusIcon className='-ml-1 mr-3 h-5 w-5' />
+                            <span className='block lg:hidden'>Agregar</span>
+                            <span className='hidden lg:block'>Agregar nuevo cliente</span>
+                        </PrimaryButton>
+                    )}
+                </div>
+                <div className='w-full rounded-md lg:bg-white px-0 lg:px-9 py-0 lg:py-7 mt-4'>
+                    {isLoadingData ? (
+                        <Table title="Clientes agregados" columns={tableColumns} actionItems={actions} isLoadingData={isLoadingData} />
+                    ) : (
+                        <>
+                            {clients.length > 0 ? (
+                                <div>
+                                    <div>
+                                        <Input
+                                            type="search"
+                                            placeholder="Buscar"
+                                            onChange={e => handleSearch(e.target.value)}
+                                            leftIcon={<SearchIcon className='w-4 h-4 text-gray-400' />}
+                                        />
+                                    </div>
+                                    <div className='w-full mt-4'>
+                                        {clientsList.length > 0 ?
+                                        <>
+                                            <Table title="Clientes agregados" data={clientsList} columns={tableColumns} actionItems={actions} />
+                                            <div className="lg:flex-1 lg:flex items-center justify-between pt-3">
+                                                {pagination && (
+                                                    <p className="select-none text-sm text-gray-700 hidden lg:block">
+                                                        Mostrando {pagination.pages.find(element => element.page == currentPage).range.join(' a ')} de {pagination.totalItems} resultados
+                                                    </p>
+                                                )}
+                                                <div>
+                                                    <Pagination pages={pagination?.totalPages || pagination?.pages?.length} currentPage={currentPage} setCurrentPage={setCurrenPage} />
+                                                </div>
+                                            </div>
+                                        </>
+                                        :
+                                            <div className='w-full'>
+                                                <EmptyState icon={<SearchIcon className='w-12 h-12' />} title='No existen coincidencias' message='Inténtalo de nuevo con otra palabra.' />
+                                            </div>
+                                        }
+                                        
+                                    </div>
+                                </div>
+                            ) : (
+                                <div className="text-center">
+                                    <EmptyState icon={<UsersIcon className='w-12 h-12' />} title='No hay clientes agregados' message='Comienza agregando clientes nuevos a la lista. Será la manerá más sencilla de facturar.' />
+                                    <div className="mt-6">
+                                        <PrimaryButton
+                                            onClick={() => setOpenClientModal(true)}>
+                                            <PlusIcon className='-ml-1 mr-3 h-5 w-5' />
+                                            Agregar nuevo cliente
+                                        </PrimaryButton>
+                                    </div>
+                                </div>
+                            )}
+                        </>
+                    )}
+                </div>
+            </div >
+            <Modal open={openClientModal} setOpen={onCancelAdded} className='w-full sm:max-w-[30rem]'>
+                <AddClient organizationId={user?.organizationId} client={editClient} onAdded={onClientAdded} onCancel={onCancelAdded} error={error} setIsLoading={setIsLoading} />
+            </Modal>
+        </>
     )
 }
 
-export default Clients
+export default Clients;
